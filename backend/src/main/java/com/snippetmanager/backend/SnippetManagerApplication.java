1d609ea431545cf50ba3881bc323c40271eba32d
Renan <renanmerlotti@gmail.com>
2026-01-20 21:33:28 -0300

    first commit

---
+++ backend/src/main/java/com/snippetmanager/backend/SnippetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.snippetmanager.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippetManagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/snippetmanager/backend/SnippetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snippetmanager.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
