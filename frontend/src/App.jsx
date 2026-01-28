b15e5f49c9c4e166bb2fe3aae2ab3a9348f9389d
Renan <renanmerlotti@gmail.com>
2026-01-27 21:08:44 -0300

    feat { iniciando a implementacao do frontend com React, Vite e Tailwind }

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-red-700 min-h-screen max-w-3xs'>
+
+    </div>
+  )
+}
+
+export default App
