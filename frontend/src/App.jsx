2958f646e226216bac4cb926c711839bc397b018
Renan <renanmerlotti@gmail.com>
2026-03-09 17:58:14 -0300

    fix { consertando import do Navigate }

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Login from './components/Login'
 import Register from './components/Register'
 import { Toaster } from 'react-hot-toast'
